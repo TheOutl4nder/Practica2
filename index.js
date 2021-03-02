7da035ea3822cfbcf1f819e85fa251ec42912f1b
The Outl4nder <theoutl4nder@gmail.com>
2021-03-01 20:59:36 -0600

    inital form with mongoDB

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const path=require('path');
 const fetch = require('node-fetch');
 const exphbs = require('express-handlebars');
 const dotenv=require('dotenv').config();
+const bodyParser=require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
 const port = process.env.PORT;
 const key = process.env.API_KEY;
+const mongouri=process.env.MONGO_URL;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false})); 
+app.use(bodyParser.json()); 
 app.use(express.static(path.join(__dirname+'/Content')));
 app.use(express.static(path.join(__dirname+'/Scripts')));
 
@@ -22,6 +28,20 @@ app.get('/form.html',(req,res)=>{
     res.sendFile(path.join(__dirname,'Views','form.html'));
 })
 
+app.post('/form.html',async (req,res)=>{
+    res.statusCode=200;
+    let form=req.body;
+    MongoClient.connect(mongouri, { useUnifiedTopology: true }, async function(err, client) {
+        if(err) {
+             console.log('Error\n',err);
+        }
+        console.log('Connected to Mongo');
+        const collection = client.db("news_app").collection("user");
+        await collection.insertOne({user: form.user, email: form.email, password: form.password});
+        client.close();
+     });
+     res.sendFile(path.join(__dirname,'Views','form.html'));
+})
 
 app.get('/search/',(req,res)=>{
     console.log('on it');
